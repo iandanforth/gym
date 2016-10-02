60f199d5be39ed9c52754e480134d8d458065dde
idanforth <iandanforth@gmail.com>
2016-10-02 14:09:07 -0700

    Add comments and do a bit of cleaning

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -41,24 +41,34 @@ class CartPoleEnv(gym.Env):
 
     def _step(self, action):
         action = action
-        assert action==0 or action==1, "%r (%s) invalid"%(action, type(action))
+        assert action == 0 or action == 1, "%r (%s) invalid" % (action, type(action))
+
+        # Copy existing state and unpack
         state = self.state
         x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action==1 else -self.force_mag
+
+        # Establish force and direction from action
+        force = self.force_mag if action == 1 else -self.force_mag
+
+        # Update physics
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
         temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
+        thetaacc = (self.gravity * sintheta - costheta * temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
         xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        x  = x + self.tau * x_dot
+        x = x + self.tau * x_dot
         x_dot = x_dot + self.tau * xacc
         theta = theta + self.tau * theta_dot
         theta_dot = theta_dot + self.tau * thetaacc
-        self.state = (x,x_dot,theta,theta_dot)
-        done =  x < -self.x_threshold \
-                or x > self.x_threshold \
-                or theta < -self.theta_threshold_radians \
-                or theta > self.theta_threshold_radians
+
+        # Store new state
+        self.state = (x, x_dot, theta, theta_dot)
+
+        # Determine if this episode is terminal
+        done = x < -self.x_threshold \
+            or x > self.x_threshold \
+            or theta < -self.theta_threshold_radians \
+            or theta > self.theta_threshold_radians
         done = bool(done)
 
         if not done:
@@ -73,6 +83,7 @@ class CartPoleEnv(gym.Env):
             self.steps_beyond_done += 1
             reward = 0.0
 
+        # Return observation, reward, done, info
         return np.array(self.state), reward, done, {}
 
     def _reset(self):
