e61cd09e05b8317df4297d4e6808c3c99b326b60
idanforth <iandanforth@gmail.com>
2016-10-02 14:10:18 -0700

    Update test script for cannon cartpole

--- first.py
+++ first.py
@@ -6,11 +6,17 @@ if __name__ == '__main__':
 
     env = gym.make('CannonCartPole-v0')
     env.reset()
-    env.render()
     startTime = time.time()
-    for i in xrange(60 * 40):
-        # time.sleep(290)
-        env.step(i)
+    for i in xrange(60 * 5):
+        # time.sleep(0.2)
+        if i % 2 == 0:
+            action = 1
+        else:
+            action = 0
+        observation, reward, done, info = env.step(action)
+        # if done == True:
+        #     break
+        print observation, reward, done, info
         # if i > 410:
         #     time.sleep(20)
     print time.time() - startTime
